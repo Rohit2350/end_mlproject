bc233f592d2c15a4873411b1a55476b87cdfc6cb
Rohit2350 <77195677+Rohit2350@users.noreply.github.com>
2024-05-12 12:26:08 +0530

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,10 @@
-import logging 
+import logging
 import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)   ##keeps appending new log file
+os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
@@ -15,6 +15,3 @@ logging.basicConfig(
 
 
 )
-
-# if __name__=="__main__":
-    # logging.info('logging has started')
